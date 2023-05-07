a30eee1ea4aecd8b71b2ec3ea8f6ba3256fd90d3
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-05-07 11:20:29 +0530

    word guessing game

--- hello.py
+++ hello.py
@@ -1,26 +1,22 @@
-import math
 import random
 
 
-def mathRand():
-    randomNumb = random.randint(0, 50)
-    print(randomNumb)
-    count = 0
-    while (count != 5):
-        inputOfUser = input("Enter Your Guessed Number : ")
-        type_conv = int(inputOfUser)
-        if (type_conv == randomNumb):
-            print('You Guessed The Right Now')
-            print(randomNumb)
-            break
-        elif (type_conv < randomNumb):
-            print('Oops You Guessed The Wrong Number')
-            print("Hint : Go Bigger")
-        elif (type_conv > randomNumb):
-            print('Oops You Guessed The Wrong Number')
-            print("Hint : Go Smaller")
+def wordRandomizer():
+    wordArray = ['Teflon', 'Armonian', 'Agam', 'Panchal', 'Bhavya', 'Godzilla']
+    chosenByRob = random.choice(wordArray)
 
+    count = 5
+
+    while (count != 0):
+        inputOfUser = input('Enter Your Guessed Word : ')
+
+        if (inputOfUser != chosenByRob):
+            print('Oof Try Again')
+            continue
+        else:
+            print('Correct')
+            break
     count += 1
 
 
-mathRand()
+wordRandomizer()
